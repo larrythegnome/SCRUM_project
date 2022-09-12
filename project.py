94409a0c214953c185288dc442e90705a606b2e4
larrythegnome <logankeaton5@gmail.com>
2022-09-12 13:16:41 -0400

    first commit

---
+++ project.py
@@ -0,0 +1,11 @@
+customer_name = input("Welcome, what is your name? ")
+starting_balance = 5000.25
+
+print (f"Hello {customer_name} you're starting balance is {starting_balance}")
+
+pay_check = input("How much of your paycheck would you like to deposit? ")
+expenditure_item = input("Looks like you went shopping. What did you buy? ")
+expenditure = input("How much does a banjo cost: ")
+
+def checking_balance(user_name, balance, deposits, expense_item, expense_amount):
+    
